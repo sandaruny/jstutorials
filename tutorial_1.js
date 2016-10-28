eb68084a341eb781c2023d2b90d8350849529b4e
sandaruny <sandaru.ny@gmail.com>
2016-10-28 19:44:41 +0530

    grammer finished

--- tutorial_1.js
+++ tutorial_1.js
@@ -174,6 +174,117 @@ if (true) {
 }
 log(x); // 2
 //24/////////////////
+function foo() {
+	var x = 1; log(x) //1
+	if (x) {
+		(function () {
+			var x = 2;log(x) //1
+			// some other code
+		}());
+	}
+	log(x) //2
+	// x is still 1.
+}
+foo();
+//25//////////////////
+function test() {
+	foo(); // TypeError "foo is not a function"
+	bar(); // "this will run!"
+	var foo = function () { // function expression assigned to local variable 'foo'
+		alert("this won't run!");
+	}
+	function bar() { // function declaration, given the name 'bar'
+		alert("this will run!");
+	}
+}
+test();
+//////////////////////
+//Literals
+//26//////////////////
+var fish = ["Lion", , "Angel"];
+log(fish[1]) // 3 length 
+//27//////////////////
+var list = ['home', , 'school', , ];
+log(list.length) // 4
+log(list[6]) // undefined
+//27///////////////////
+var sales = "Toyota";
+
+function carTypes(name) {
+  if (name === "Honda") {
+    return name;
+  } else {
+    return "Sorry, we don't sell " + name + ".";
+  }
+}
+
+var car = { myCar: "Saturn", getCar: carTypes("Honda"), special: sales };
+
+console.log(car.myCar);   // Saturn
+console.log(car.getCar);  // Honda // this is not the function
+console.log(car.special); // Toyota
+//27////////////////////
+var unusualPropertyNames = {
+  "": "An empty string",
+  "!": "Bang!"
+}
+console.log(unusualPropertyNames."");   // SyntaxError: Unexpected string
+console.log(unusualPropertyNames[""]);  // An empty string
+console.log(unusualPropertyNames.!);    // SyntaxError: Unexpected token !
+console.log(unusualPropertyNames["!"]); // Bang!
+//28////////////////////
+var obj = {
+    // __proto__
+    __proto__: theProtoObj,
+    // Shorthand for ‘handler: handler’
+    handler,
+    // Methods
+    toString() {
+     // Super calls
+     return "d " + super.toString();
+    },
+    // Computed (dynamic) property names
+    [ 'prop_' + (() => 42)() ]: 42
+};
+//29/////////////////////
+var foo = {a: "alpha", 2: "two"};
+console.log(foo.a);    // alpha
+console.log(foo[2]);   // two
+//console.log(foo.2);  // Error: missing ) after argument list
+//console.log(foo[a]); // Error: a is not defined
+console.log(foo["a"]); // alpha
+console.log(foo["2"]); // two
+
+// RegExp literals
+//30//////////////////////
+var re = /ab+c/;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
