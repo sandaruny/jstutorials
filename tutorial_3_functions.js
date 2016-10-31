2d91dfcf25336e6199f5611a540a8a739d1c100a
sandaruny <sandaru.ny@gmail.com>
2016-10-31 16:35:37 +0530

    Strings

--- tutorial_3_functions.js
+++ tutorial_3_functions.js
@@ -152,11 +152,157 @@ function outside() {
 }
 result = outside()(20); // returns 20 instead of 10. inner takes the precedence
 //14/////////////////
-
+var pet = function(name) {   // The outer function defines a variable called "name"
+  var getName = function() {
+    return name;             // The inner function has access to the "name" variable of the outer function
+  }
+  return getName;            // Return the inner function, thereby exposing it to outer scopes
+},
+myPet = pet("Vivie");
+myPet();                     // Returns "Vivie"
 //15/////////////////
+var createPet = function(name) {
+  var sex;
+  
+  return {
+    setName: function(newName) {
+      name = newName;
+    },
+    
+    getName: function() {
+      return name;
+    },
+    
+    getSex: function() {
+      return sex;
+    },
+    
+    setSex: function(newSex) {
+      if(typeof newSex === "string" && (newSex.toLowerCase() === "male" || newSex.toLowerCase() === "female")) {
+        sex = newSex;
+      }
+    }
+  }
+}
+
+var pet = createPet("Vivie");
+pet.getName();                  // Vivie
+pet.setName("Oliver");
+pet.setSex("male");
+pet.getSex();                   // male
+pet.getName();                  // Oliver
 //16/////////////////
+var getCode = (function(){
+  var secureCode = "0]Eal(eh&2";    // A code we do not want outsiders to be able to modify...
+  
+  return function () {
+    return secureCode;
+  };
+})();
+getCode();    // Returns the secureCode
+//16/////////////////
+function myConcat(separator) {
+   var result = ""; // initialize list
+   var i;
+   // iterate through arguments
+   for (i = 1; i < arguments.length; i++) {
+      result += arguments[i] + separator;
+   }
+   return result;
+}
+// returns "red, orange, blue, "
+myConcat(", ", "red", "orange", "blue");
+// returns "elephant; giraffe; lion; cheetah; "
+myConcat("; ", "elephant", "giraffe", "lion", "cheetah");
+// returns "sage. basil. oregano. pepper. parsley. "
+myConcat(". ", "sage", "basil", "oregano", "pepper", "parsley");
 
+//Parameters
+//17//////////////////
+function multiply(a, b) {
+  b = typeof b !== 'undefined' ?  b : 1; 
+  return a*b;
+}
+multiply(5); // 5 
+//18///////////////////
+function multiply(a, b = 1) { //EC6 way of defining degfault value
+  return a*b;
+}
+multiply(5); // 5
+//19//////////////////
+function multiply(multiplier, ...theArgs) {
+  return theArgs.map(x => multiplier * x);
+}
+var arr = multiply(2, 1, 2, 3);
+console.log(arr); // [2, 4, 6]
+//Arrow Functions
+//20//////////////////
+var a = [
+  "Hydrogen",
+  "Helium",
+  "Lithium",
+  "Beryl­lium"
+];
 
+var a2 = a.map(function(s){ return s.length });
+
+var a3 = a.map( s => s.length );
+//This keyword
+//23///////////////////
+function Person() {
+  "use strict"
+  this.age = "5";
+  console.log(this === window);
+}
+window.Person() // true
+window.age // "5" it refers to the window objec
+
+//24///////////////////
+function Person() {
+  "use strict"
+  this.age = "5";
+  console.log(this === window);
+}
+Person() // true
+window.age // undefined ; the use strict will enforce to call from an object
+//25///////////////////
+var Person = {age:0, incre: function(){
+  this.age ++;
+}}
+Person.incre();
+//26///////////////////
+function f(){
+  return this.a;
+}
+var g = f.bind({a:"azerty"});
+console.log(g()); // azerty
+var o = {a:37, f:f, g:g};
+console.log(o.f(), o.g()); // 37, azerty
+//27///////////////////
+var globalObject = this;
+var foo = (() => this);
+console.log(foo() === globalObject); // true
+//28///////////////////
+// Call as a method of an object
+var obj = {foo: foo};
+console.log(obj.foo() === globalObject); // true
+
+// Attempt to set this using call
+console.log(foo.call(obj) === globalObject); // true
+
+// Attempt to set this using bind
+foo = foo.bind(obj);
+console.log(foo() === globalObject); // true
+//28////////////////////
+function Person(){
+  this.age = 0;
+
+  setInterval(() => {
+    this.age++; // |this| properly refers to the person object
+  }, 1000);
+}
+var p = new Person();
+//29/////////////////////
 
 
 
